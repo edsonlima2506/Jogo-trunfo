f919e6bd8581d8777b5b9dd0e4420aa13cd8f4da
Edson Lima <edsoneduardo526@gmail.com>
2021-10-13 15:49:09 -0300

    Requisito 1 (Take 2)

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,11 +10,13 @@ class Form extends React.Component {
         <input type="number" data-testid="attr2-input" />
         <input type="number" data-testid="attr3-input" />
         <input type="text" data-testid="image-input" />
+
         <select data-testid="rare-input">
           <option value="normal" id="normal">Normal</option>
           <option value="raro" id="normal">Raro</option>
           <option value="muito raro" id="normal">Muito raro</option>
         </select>
+
         <input type="checkbox" data-testid="trunfo-input" />
         <button data-testid="save-button" type="submit">Criar</button>
       </form>
